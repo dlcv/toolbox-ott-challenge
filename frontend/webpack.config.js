6ed61cd548b437240a8782b538f009a030a05e7b
Francisco De La Cruz <dlcv@live.com.ve>
2026-05-21 18:15:51 -0400

    Feat: gitignore files and installations

---
+++ frontend/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true,
+    historyApiFallback: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ]
+}
